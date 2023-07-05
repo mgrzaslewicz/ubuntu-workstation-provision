3cd64bd7d71126821793bb34270ebcae41cb5e78
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-07-05 22:40:42 +0200

    Make it possible to run only java related tasks
    To make testing subset of provisioning easier and devloop quicker

--- provision.sh
+++ provision.sh
@@ -2,9 +2,11 @@
 set -x
 
 SKIP_TAGS=${SKIP_TAGS:-"non-existing-tag"}
+TAGS=${TAGS:-"all"}
 
 ansible-playbook -K \
 --connection=local \
 --inventory 127.0.0.1, \
 --skip-tags "${SKIP_TAGS}" \
+--tags "${TAGS}" \
 --limit 127.0.0.1 playbooks/provision.yml
